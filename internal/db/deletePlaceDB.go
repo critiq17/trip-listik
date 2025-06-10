481599b7c4971ffef9bcbed36329e4aaa35bb8fc
critiq17 <critiq17@gmail.com>
2025-06-10 14:45:59 +0300

    second debug DB

--- internal/db/deletePlaceDB.go
+++ internal/db/deletePlaceDB.go
@@ -4,6 +4,6 @@ import "context"
 
 func DeletePlaceFromDB(chatID int64, place string) error {
 	_, err := DB.Exec(context.Background(),
-		"DELETE FROM user-wishlist WHERE chat_id = $1 AND place = $2", chatID, place)
+		"DELETE FROM wishlist WHERE chat_id = $1 AND place = $2", chatID, place)
 	return err
 }
