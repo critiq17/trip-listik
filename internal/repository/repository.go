800b71642b478832118b0efd2ecafa0da4ed2feb
critiq17 <critiq17@gmail.com>
2025-10-30 09:58:24 +0100

    fix service and repo

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"log"
+
 	"github.com/critiq17/tripListik/internal/models"
 	"gorm.io/gorm"
 )
@@ -45,6 +47,22 @@ func (r *Repository) GetPlacesByUserID(userID uint) ([]models.Place, error) {
 	var places []models.Place
 	result := r.DB.Where("user_id = ?", userID).Find(&places)
 	return places, result.Error
+}
+
+func (r *Repository) GetOrCreateUser(telegramID int64, firstName, userName string) (models.User, error) {
+
+	log.Printf("DEBUG: AddPlace called with telegramID=%d, firstName=%s, userName=%s", telegramID, firstName, userName)
+
+	var user models.User
+
+	result := r.DB.
+		Where(models.User{TelegramID: telegramID}). // 👈 вот это важно
+		Attrs(models.User{
+			FirstName: firstName,
+			UserName:  userName,
+		}).
+		FirstOrCreate(&user)
+	return user, result.Error
 }
 
 func (r *Repository) AddPlaceToUser(userID uint, placeName string) error {
