37c3536d69e7be95507520b28983939527dc841f
critiq17 <critiq17@gmail.com>
2026-03-23 00:16:30 +0100

    create landing

---
+++ landing/.svelte-kit/output/server/manifest.js
@@ -0,0 +1,32 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.svg","robots.txt"]),
+	mimeTypes: {".svg":"image/svg+xml",".txt":"text/plain"},
+	_: {
+		client: {start:"_app/immutable/entry/start.BqZ2LhMO.js",app:"_app/immutable/entry/app.DVV5x80F.js",imports:["_app/immutable/entry/start.BqZ2LhMO.js","_app/immutable/chunks/BiYGH3oD.js","_app/immutable/chunks/CoJjOMgx.js","_app/immutable/chunks/BXYGMuBD.js","_app/immutable/entry/app.DVV5x80F.js","_app/immutable/chunks/CoJjOMgx.js","_app/immutable/chunks/DiKKL0P5.js","_app/immutable/chunks/BD95CvEF.js","_app/immutable/chunks/BXYGMuBD.js","_app/immutable/chunks/DWTmh51L.js","_app/immutable/chunks/lpPEBbVQ.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		nodes: [
+			__memo(() => import('./nodes/0.js')),
+			__memo(() => import('./nodes/1.js'))
+		],
+		remotes: {
+			
+		},
+		routes: [
+			
+		],
+		prerendered_routes: new Set(["/"]),
+		matchers: async () => {
+			
+			return {  };
+		},
+		server_assets: {}
+	}
+}
+})();
---
+++ landing/.svelte-kit/output/server/nodes/0.js
@@ -0,0 +1,8 @@
+
+
+export const index = 0;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/0.XLwS7vsx.js","_app/immutable/chunks/BD95CvEF.js","_app/immutable/chunks/CoJjOMgx.js","_app/immutable/chunks/lpPEBbVQ.js","_app/immutable/chunks/2FgVoid_.js"];
+export const stylesheets = ["_app/immutable/assets/0.Dw7efntF.css"];
+export const fonts = [];
